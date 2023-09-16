95ebdb956ea2b05f402fd7c3c3bc554880f02e21
N3v1 <n3v1010@gmail.com>
2023-09-16 16:35:37 +0200

    Config

---
+++ CodeMaster/CodeMaster/CodeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeMasterApp.swift
+//  CodeMaster
+//
+//  Created by Nevio Hirani on 16.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CodeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeMaster/CodeMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeMaster
+//
+//  Created by Nevio Hirani on 16.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
